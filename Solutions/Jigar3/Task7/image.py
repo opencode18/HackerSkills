250e5d07766ceaad3fcc0ed98989a7e84f41e1d2
Jigar <Chavada.jigar.3@gmail.com>
2018-01-15 16:11:13 +0530

    Done requested changes

--- Solutions/Jigar3/Task7/image.py
+++ Solutions/Jigar3/Task7/image.py
@@ -2,12 +2,9 @@ from PIL import ImageFont, ImageDraw, Image
 
 
 print("NOTE: Please place the image in the same folder as the one in which the script is running")
+print("NOTE: Name the text file to be read as names.txt")
+
 imName = raw_input("Input the name of the image along with its extension type\n")
-
-im = Image.open(imName)
-draw = ImageDraw.Draw(im)
-
-text = raw_input("Enter the text to be added:\n")
 
 print("Enter the co-ordinates of the text to be placed")
 x = int(raw_input("X co-ordinate:\n"))
@@ -16,7 +13,16 @@ y = int(raw_input("Y co-ordinate:\n"))
 fontName = raw_input("Enter the name of the font file which is to be used:\n")
 fontSize = int(raw_input("Enter the size of font:\n"))
 
-font = ImageFont.truetype(fontName, fontSize)
-draw.text((x, y), text, font=font)
+with open("names.txt") as f:
+    for line in f:
+        text = line.rstrip()
 
-im.save('certificate.jpg')
+
+        im = Image.open(imName)
+        draw = ImageDraw.Draw(im)
+
+        font = ImageFont.truetype(fontName, fontSize)
+        draw.text((x, y), text, font=font)
+
+        im.save(text + '.jpg')
+        print("Finished with " + text + ".jpg")
