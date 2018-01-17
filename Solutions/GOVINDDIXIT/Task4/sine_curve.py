e7f940aedcb6ef4dab0dc1bc59c6645e5cc865d2
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-01-17 14:43:30 +0530

    Update sine_curve.py

--- Solutions/GOVINDDIXIT/Task4/sine_curve.py
+++ Solutions/GOVINDDIXIT/Task4/sine_curve.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.arange(0.0,2.0,0.01)
-y=2*np.sin(np.pi/4*x)
+y=2*np.sin((np.pi/4)*x)
 plt.plot(x,y)
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
