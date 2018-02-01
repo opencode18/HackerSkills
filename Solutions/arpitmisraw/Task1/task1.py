5ba3dc41919d3733c745723b070a372527fb2921
arpit <tipra2614@gmail.com>
2018-02-01 23:41:02 +0530

    Added Arpit's solution to Task 1

---
+++ Solutions/arpitmisraw/Task1/task1.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+description = input("Enter Description: ")
+location = input("Enter Location: ")
+
+url = "https://jobs.github.com/positions?description=" + "python" + "&location=" + "new+york"
+resp = requests.get(url)
+
+html = resp.text
+soup = BeautifulSoup(html)
+
+
+title = soup.select('.title h4 a')
+
+for job_title in title:
+    print("\n\n" + job_title.getText() + "\n")
+    url_job = "https://jobs.github.com" + job_title.get('href')
+    job_req = requests.get(url_job)
+    job_html = job_req.text
+    job_soup = BeautifulSoup(job_html)
+    info = job_soup.select('.column.main')
+    link = job_soup.select('.highlighted a')
+    for job_info in info:
+        print(job_info.getText())
+    print("\nApply here: " + link[0].get('href'))
+    print("\n\n")
+
