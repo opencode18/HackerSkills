66ffabf037017dae439ce55df169a165910a011e
Ace-Bansal <bansalekansh@gmail.com>
2018-02-07 10:07:32 +0530

    Add solutions for task 2 & 3.

---
+++ Solutions/Ace-Bansal/task3.py
@@ -0,0 +1,16 @@
+import bs4 as bs;
+import requests;
+
+source=requests.get('https://scirate.com/');
+pageHTML=BeautifulSoup(source.content,"html.parser");
+
+articleTitle=pageHTML.find_all('div',class_='title');
+articleLink=pageHTML.find_all('a',attrs={'title': "Download PDF"});
+
+length=len(articleTitle);
+for i in range(length):
+    print(str(i+1)+")\n");
+    print("ARTICLE TITLE:"+articleTitle[i].text);
+    print("ARTICLE LINK:"+articleLink[i].get('href'));
+    print("\n");
+    print("=============================================================================\n");
