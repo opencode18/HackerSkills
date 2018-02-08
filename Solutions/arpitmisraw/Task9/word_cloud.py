280bc64583b62cab325e9e5125a039eb23dce976
arpit <tipra2614@gmail.com>
2018-02-08 23:56:03 +0530

    Added Arpit's solution to Task9

---
+++ Solutions/arpitmisraw/Task9/word_cloud.py
@@ -0,0 +1,29 @@
+import wikipedia
+from wordcloud import WordCloud, STOPWORDS
+import os
+from PIL import Image
+import numpy as np
+
+
+dir = os.path.dirname(__file__)
+
+
+def wiki(query):
+    title = wikipedia.search(query)[0]
+    page = wikipedia.page(title)
+    return page.content
+
+
+def word_cloud(text):
+    mask = np.array(Image.open(os.path.join(dir, 'dc.jpg')))
+    stop_words = set(STOPWORDS)
+    wc = WordCloud(background_color = 'white', mask = mask, max_words = 200, stopwords = stop_words)
+    wc.generate(text)
+    wc.to_file(os.path.join(dir, 'wc.png'))
+
+
+word_cloud(wiki('Dunkirk (2017 film)'))
+
+
+
+
